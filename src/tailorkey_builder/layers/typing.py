0bbd4d4f12aec21ff9921ef32ef89e1bc71ef3b1
Bas Nijholt <bas@nijho.lt>
2025-11-08 11:10:47 -0800

    Represent typing layer as sparse KeySpec

--- src/tailorkey_builder/layers/typing.py
+++ src/tailorkey_builder/layers/typing.py
@@ -2,10 +2,23 @@
 
 from __future__ import annotations
 
-from .base import Layer, copy_layer, load_layer_from_data
+from .base import KeySpec, Layer, LayerSpec, build_layer_from_spec, copy_layer
 
 
-_BASE_TYPING_LAYER: Layer = load_layer_from_data("Typing", filename="typing_layer.json")
+TYPING_LAYER_SPEC = LayerSpec(
+    overrides={
+        35: KeySpec("&kp", (KeySpec("A"),)),
+        36: KeySpec("&kp", (KeySpec("S"),)),
+        37: KeySpec("&kp", (KeySpec("D"),)),
+        38: KeySpec("&kp", (KeySpec("F"),)),
+        41: KeySpec("&kp", (KeySpec("J"),)),
+        42: KeySpec("&kp", (KeySpec("K"),)),
+        43: KeySpec("&kp", (KeySpec("L"),)),
+        44: KeySpec("&kp", (KeySpec("SEMI"),)),
+    }
+)
+
+_BASE_TYPING_LAYER: Layer = build_layer_from_spec(TYPING_LAYER_SPEC)
 
 
 def build_typing_layer(_variant: str) -> Layer:
