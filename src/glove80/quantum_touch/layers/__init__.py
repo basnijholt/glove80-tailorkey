7fdc0d8292eeabb1c31725bc91b637f0073e435b
Bas Nijholt <bas@nijho.lt>
2025-11-08 12:22:32 -0800

    Add QuantumTouch Original layer spec

--- src/glove80/quantum_touch/layers/__init__.py
+++ src/glove80/quantum_touch/layers/__init__.py
@@ -5,6 +5,7 @@ from typing import Callable, Dict
 from ...base import Layer, LayerMap
 from .base_layer import build_base_layer
 from .hrm import build_hrm_layer
+from .original_layer import build_original_layer
 
 LayerBuilder = Callable[[str], Layer]
 
@@ -12,6 +13,7 @@ LayerBuilder = Callable[[str], Layer]
 LAYER_BUILDERS: Dict[str, LayerBuilder] = {
     "Base": build_base_layer,
     "HRM": build_hrm_layer,
+    "Original": build_original_layer,
 }
 
 
